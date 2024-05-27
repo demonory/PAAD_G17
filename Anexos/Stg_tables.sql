8e955ef4ba66827b4f7462ede5324506ba612e2b
dmonroy <129700342+demonory@users.noreply.github.com>
2024-05-26 19:29:58 -0500

    Delete Anexos/Stg_tables.sql

--- Anexos/Stg_tables.sql
+++
@@ -1,8 +0,0 @@
-USE paad_g17;
-
-select * from stg_baq_compras limit 100;
-select * from stg_products_baq limit 100;
-select * from stg_revision_precios_sipsa limit 100;
-select * from stg_waste_percentage_by_age limit 100;
-select * from stg_datos_con_clusters limit 100;
-select * from stg_descripcion_clusters_proveedores  limit 100;
