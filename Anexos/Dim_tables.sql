b58556a134e2ad53e3818ea024ac88fa210ea61b
dmonroy <129700342+demonory@users.noreply.github.com>
2024-05-26 19:29:19 -0500

    Delete Anexos/Dim_tables.sql

--- Anexos/Dim_tables.sql
+++
@@ -1,9 +0,0 @@
-USE paad_g17;
-
-
-select  * from dim_fecha_entrada;
-select  * from dim_proveedor;
-select  * from dim_locacion;
-select * from dim_producto;
-select * from bridge_duracion;
-select * from dim_duracion;
